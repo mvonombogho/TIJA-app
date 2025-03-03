5e718a51f5ffbdc4f133e07c22912b3660dba41d
israel mvono <israelmvono@gmail.com>
2025-03-03 23:40:20 +0300

    Update MainApplication.java with Firebase initialization

--- android/app/src/main/java/com/tija/MainApplication.java
+++ android/app/src/main/java/com/tija/MainApplication.java
@@ -8,6 +8,10 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+
+// Import Firebase
+import com.google.firebase.FirebaseApp;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -52,6 +56,12 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    
+    // Initialize Firebase
+    if (FirebaseApp.getApps(this).isEmpty()) {
+      FirebaseApp.initializeApp(this);
+    }
+    
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
