d61e54fd7cdc60f13a5b057fd725c792dfac813a
israel mvono <israelmvono@gmail.com>
2025-03-03 21:55:28 +0300

    Add Firebase configuration

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from '@react-native-firebase/app';
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACg1FLsNpx6KlhAq6f_OD81znOu2rmE8M",
+  authDomain: "tija-57482.firebaseapp.com",
+  projectId: "tija-57482",
+  storageBucket: "tija-57482.firebasestorage.app",
+  messagingSenderId: "348163673220",
+  appId: "1:348163673220:web:6b9a9cd9874da425e2c6d1",
+  measurementId: "G-WZSY911HTJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
